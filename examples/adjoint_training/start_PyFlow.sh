7f3e5091caa4c39d630878e76549b161cbd4adb0
Jonathan MacArt <jmacart@illinois.edu>
2019-11-07 15:11:57 -0600

    Adding adjoint training example

---
+++ examples/adjoint_training/start_PyFlow.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ------------------------------------------------------------
+# User-configurable options
+# ------------------------------------------------------------
+
+# Name of PyFlow driver script
+PYFLOW_DRIVER=run_PyFlow_adjoint_training.py
+
+# Number of tasks (NTASKS) & number of tasks per node (NTASKS_NODE)
+#   --> For GPU runs, leave NTASKS_NODE=1
+NTASKS=2
+NTASKS_NODE=1
+
+# Path to PyFlow source code
+PYFLOW_PATH=../../
+
+
+# ------------------------------------------------------------
+# No user-configurable options below here
+# ------------------------------------------------------------
+
+# Set up PyFlow environment
+cp -r ${PYFLOW_PATH}/src .
+cd src/core
+cp ../../${PYFLOW_DRIVER} .
+
+# Call the PyFlow driver
+#   Change this to your system's default (mpirun, srun, aprun, etc.)
+mpirun -np $NTASKS python ${PYFLOW_DRIVER}
+
+# Process results
+cd ../..
